1025276d5ffb2ce0307b45db8cc6b296ffcf552f
NzSN <aydenlin@foxmail.com>
2023-07-04 15:18:49 +0800

    Add listener in demo.

--- app/TsTransformer.cc
+++ app/TsTransformer.cc
@@ -1,8 +1,31 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <algorithm>
 #include "parsers/ts/TypeScriptLexer.h"
 #include "parsers/ts/TypeScriptParser.h"
-#include "parsers/ts/TypeScriptParserListener.h"
+#include "parsers/ts/TypeScriptParserBaseListener.h"
+
+class MyListener: public TypeScriptParserBaseListener {
+public:
+  void enterImportStatement(TypeScriptParser::ImportStatementContext* ctx) {
+    std::string fromModule =
+      ctx->fromBlock()->StringLiteral()->getSymbol()->getText();
+
+    auto names = ctx->fromBlock()->multipleImportStatement()->identifierName();
+    std::vector<std::string> importSyms(names.size());
+
+    std::transform(names.cbegin(), names.cend(), importSyms.begin(),
+                   [](TypeScriptParser::IdentifierNameContext* ictx) {
+                     return ictx->Identifier()->getSymbol()->getText();
+                   });
+    std::cout << "Import symbols: ";
+    for (auto is: importSyms) {
+      std::cout << is << " ";
+    }
+    std::cout << "from " << fromModule << std::endl;
+  }
+};
 
 
 int main(int argc, char *argv[]) {
@@ -14,9 +37,18 @@ int main(int argc, char *argv[]) {
     antlr4::CommonTokenStream tokens(&lexer);
     TypeScriptParser parser(&tokens);
 
+
     antlr4::tree::ParseTree *tree = parser.program();
 
+    // Print AST
     std::cout << tree->toStringTree(&parser) << std::endl;
+
+    std::cout << std::endl << std::endl <<
+      "=============================================" <<
+      std::endl;
+
+    MyListener listener;
+    antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
 
     return 0;
 }
