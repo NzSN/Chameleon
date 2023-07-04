88c3ce824303f3f50cf84ae532c8c72f1384941f
NzSN <aydenlin@foxmail.com>
2023-07-04 13:43:00 +0800

    Print ast in ts_transformer

--- app/TsTransformer.cc
+++ app/TsTransformer.cc
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <fstream>
 #include "parsers/ts/TypeScriptLexer.h"
 #include "parsers/ts/TypeScriptParser.h"
+#include "parsers/ts/TypeScriptParserListener.h"
 
 
 int main(int argc, char *argv[]) {
+    std::ifstream stream;
+    stream.open(argv[1]);
+
+    antlr4::ANTLRInputStream input(stream);
+    TypeScriptLexer lexer(&input);
+    antlr4::CommonTokenStream tokens(&lexer);
+    TypeScriptParser parser(&tokens);
+
+    antlr4::tree::ParseTree *tree = parser.program();
+
+    std::cout << tree->toStringTree(&parser) << std::endl;
 
     return 0;
 }
