60a459976cbe5d44cb0eed67596ecce6dba23cca
NzSN <aydenlin@foxmail.com>
2023-07-10 17:07:35 +0800

    Use EXPECT_EQ instead of EXPECT_TRUE.

--- src/rules/parser/compiler/ChameleonsParser_tests.cc
+++ src/rules/parser/compiler/ChameleonsParser_tests.cc
@@ -3,30 +3,46 @@
 #include "ChameleonsLexer.h"
 #include "ChameleonsParser.h"
 
-std::string GetParseTree(std::string prog) {
-    std::stringstream ss("R:{ Origin }=>{ Migrated }");
+std::string GetParseTree(std::string prog, bool pretty = false) {
+    std::stringstream ss(prog);
     antlr4::ANTLRInputStream input(ss);
     ChameleonsLexer lexer(&input);
     antlr4::CommonTokenStream tokens(&lexer);
     ChameleonsParser parser(&tokens);
 
     antlr4::tree::ParseTree *tree = parser.migrate();
-    return tree->toStringTree(&parser);
+    return tree->toStringTree(&parser, pretty);
 }
 
-struct ChameleonsTest: public ::testing::Test {
-    void SetUp() override {}
 
-    void TearDown() override {}
-};
-
-
-TEST_F(ChameleonsTest, Basic) {
-    std::string treeStr = GetParseTree("R: { Origin } => { Target }");
+TEST(ChameleonsTest, Basic) {
+    std::string treeStr = GetParseTree("R: { Origin } => { Migrated }");
     std::string expected =
         "(migrate "
         "(migrateRules "
         "(migrateRule "
         "R : { (originCode  Origin ) } => { (targetCode  Migrated ) })))";
-    EXPECT_TRUE(treeStr == expected);
+    EXPECT_EQ(treeStr, expected);
+}
+
+TEST(ChameleonsTest, WithSpaceCodes) {
+    std::string treeStr = GetParseTree(
+        "R: {"
+
+        "setTimeout(%A, %B);"
+        "console.log(%A, %B); "
+
+        " } => { "
+
+        "SetTimeout_Coro(%A, %B)"
+
+        " }");
+    std::string expected =
+        "(migrate "
+        "(migrateRules "
+        "(migrateRule "
+        "R : { (originCode setTimeout(%A, %B);console.log(%A, %B);  ) } "
+        "=> { (targetCode  SetTimeout_Coro(%A, %B) ) })))";
+
+    EXPECT_EQ(treeStr, expected);
 }
