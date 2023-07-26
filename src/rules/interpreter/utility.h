83a3b3238668985709f8aa078d878b81845e9b4c
NzSN <aydenlin@foxmail.com>
2023-07-26 12:02:25 +0800

    Use PBT instead of example testing.

--- src/rules/interpreter/utility.h
+++ src/rules/interpreter/utility.h
@@ -10,14 +10,18 @@
 namespace Rules::Interpreter::Utility {
 
 template<typename T, typename E>
-concept RangeAndPusable = std::ranges::range<T> && requires(T t, E e) {
-  t.push_back(e);
+concept RangeTuple = std::ranges::range<T> &&
+  requires(T t, std::tuple<E, E> pair) {
+    t.push_back(pair);
 };
 
-template<typename E, RangeAndPusable<E> T, RangeAndPusable<E> RT>
-RT zip_vec(T l, T r) {
+template<typename T, typename E>
+concept Range = std::ranges::range<T>;
+
+template<typename E, RangeTuple<E> RT, Range<E> T>
+RT zip(T l, T r) {
     if (l.size() != r.size()) {
-        return std::nullopt;
+      return RT{};
     }
 
     auto iter_l = std::begin(l);
@@ -25,15 +29,19 @@ RT zip_vec(T l, T r) {
 
     RT zipped{};
 
-    do {
+    while (iter_l != std::end(l)) {
         zipped.push_back({*iter_l, *iter_r});
 
-        iter_l++;
-        iter_r++;
-    } while (iter_l != std::end(l) && iter_r != std::end(r));
+        ++iter_l;
+        ++iter_r;
+    }
 
     return zipped;
 }
+
+template<typename T>
+const auto& zip_vector =
+  zip<T, std::vector<std::tuple<T, T>>, const std::vector<T>&>;
 
 } // Rules::Interpreter::Utility
 
--- src/rules/interpreter/utility_unittests.cc
+++ src/rules/interpreter/utility_unittests.cc
@@ -1,28 +1,61 @@
 #include <gtest/gtest.h>
 #include <optional>
 #include <tuple>
+#include <iostream>
 #include "utility.h"
 #include "rapidcheck.h"
 #include "rapidcheck/gtest.h"
 
 namespace Rules::Interpreter::Utility {
 
+struct ZipTest: public ::testing::Test {
+  std::vector<int> rdValVector(int size) {
+    std::vector<int> v;
+
+    for (int i = 0; i < size; ++i) {
+      v.push_back(*rc::gen::inRange(0, 100));
+    }
+
+    return v;
+  }
+
+  std::vector<int> l{}, r{};
+};
+
 // Zipping testing
-RC_GTEST_PROP(Zip, AA, (const std::string &str)) {
-  const auto strCopy = str;
-  RC_ASSERT(strCopy == str);
+RC_GTEST_FIXTURE_PROP(ZipTest, EqualSize, ()) {
+  const int size = *rc::gen::inRange(0, 10);
+
+  // Create Vectors
+  l = rdValVector(size);
+  r = rdValVector(size);
+
+  auto zipped = zip_vector<int>(l, r);
+
+  // Check Property:
+  // size(l) == size(r) == size(zipped)
+  // l[n] * r[n] == fst(zipped[n]) * snd(zipped[n])
+  RC_ASSERT(l.size() == r.size() && r.size() == zipped.size());
+  for (int index = 0; index < size; ++index) {
+    RC_ASSERT(l[index] * r[index] ==
+              std::get<0>(zipped[index]) * std::get<1>(zipped[index]));
+  }
 }
 
-TEST(UtilityTest, zip) {
-  std::vector<int> l  {1, 3, 5, 7, 9};
-  std::vector<int> r  {2, 4, 6, 8, 10};
+RC_GTEST_FIXTURE_PROP(ZipTest, DifferentSize, ()) {
+  const int size_l = *rc::gen::inRange(0, 100);
+  const int size_r = *rc::gen::inRange(0, 100);
 
-  auto zipped = zip_vec(l, r);
-  EXPECT_TRUE(zipped != std::nullopt);
-  std::vector<std::tuple<int, int>> m = {
-    {1,2}, {3,4}, {5,6}, {7,8}, {9,10}
-  };
-  EXPECT_EQ(m, zipped.value());
+  if (size_l == size_r) {
+    return;
+  }
+
+  l = rdValVector(size_l);
+  r = rdValVector(size_r);
+
+  auto zipped = zip_vector<int>(l, r);
+
+  RC_ASSERT(zipped.size() == 0);
 }
 
 } // Rules::Interpreter::Utility
