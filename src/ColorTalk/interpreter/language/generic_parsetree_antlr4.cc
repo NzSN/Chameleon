ce47815a0a354c3f4c1fecbb83e93822ca84bd1a
NzSN <aydenlin@foxmail.com>
2023-07-31 15:28:23 +0800

    Implement Antlr4Node::operator==.

---
+++ src/ColorTalk/interpreter/language/generic_parsetree_antlr4.cc
@@ -0,0 +1,9 @@
+#include "generic_parsetree_antlr4.h"
+
+namespace Rules::Interpreter::Language {
+
+bool Antlr4Node::operator==(const Antlr4Node& node) const {
+  return true;
+}
+
+} // Rules::Interpreter::Language
