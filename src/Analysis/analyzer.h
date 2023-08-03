457574a5230b48f865d0179c17afc4ac79b09f34
NzSN <aydenlin@foxmail.com>
2023-08-04 01:46:47 +0800

    Add Analyzer
    Which responsible for gather information from
    parsetree.

---
+++ src/Analysis/analyzer.h
@@ -0,0 +1,20 @@
+
+#ifndef ANALYZER_H
+#define ANALYZER_H
+
+#include "Concepts/n_ary_tree.h"
+
+namespace Analyzer {
+
+using Concepts::NAryTree::NAryTree;
+
+template<NAryTree T>
+class Analyzer {
+public:
+private:
+};
+
+} // Analyzer
+
+
+#endif /* ANALYZER_H */
