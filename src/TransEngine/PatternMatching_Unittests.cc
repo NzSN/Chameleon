6cf4564fddfcebc7f3a2c9d7538132a902576926
NzSN <aydenlin@foxmail.com>
2023-10-13 16:14:37 +0800

    Add PatternMatching Unittests.

--- src/TransEngine/PatternMatching_Unittests.cc
+++ src/TransEngine/PatternMatching_Unittests.cc
@@ -2,6 +2,7 @@
 #include <rapidcheck/gtest.h>
 #include <sstream>
 
+#include "PatternMatching.h"
 #include "SigmaTerm.h"
 #include "utility.h"
 #include "Base/generic_parsetree_antlr4.h"
@@ -34,6 +35,7 @@ RC_GTEST_FIXTURE_PROP(PatternMatchingTests, MapPatternToGPT, ()) {
   // Building a Pattern base on ParseTree,
   // without term variables.
   TransEngine::Pattern<Base::Antlr4Node> pattern(t);
+  TransEngine::SigmaTerm<Base::Antlr4Node> sigmaterm(t);
 
   // Assert that a pattern from a concrete parsetree
   // is isomorhpic to the parsetree.
@@ -41,6 +43,28 @@ RC_GTEST_FIXTURE_PROP(PatternMatchingTests, MapPatternToGPT, ()) {
     (Concepts::NAryTree::isIsomorphic<
      Base::GenericParseTree<Base::Antlr4Node>,
      TransEngine::Pattern<Base::Antlr4Node>>(pattern, t)));
+
+  RC_ASSERT(
+    (Concepts::NAryTree::isIsomorphic<
+     Base::GenericParseTree<Base::Antlr4Node>,
+     TransEngine::SigmaTerm<Base::Antlr4Node>>(sigmaterm, t)));
+}
+
+RC_GTEST_FIXTURE_PROP(PatternMatchingTests, Matching, ()) {
+  std::istringstream codes{testExpr};
+
+  auto t = Parser::Parser<
+    antlr4::tree::ParseTree*,
+    Parser::TestLangExt,
+    Base::Antlr4Node,
+    Base::GenericParseTree<Base::Antlr4Node>::TESTLANG>
+    ::parse(&codes);
+
+  TransEngine::Pattern<Base::Antlr4Node> pattern(t);
+  TransEngine::SigmaTerm<Base::Antlr4Node> sigmaterm(t);
+
+  // Try to maching the pattern and the sigmaterm
+  RC_ASSERT(patternMatching<Base::Antlr4Node>(pattern, sigmaterm));
 }
 
 } // Algorithms
