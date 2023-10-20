813d3a9b99e7b263d1d3f9af417a9782ded720cd
NzSN <aydenlin@foxmail.com>
2023-10-20 14:47:01 +0800

    Implement Expression Tree.

--- src/ExprTree.h
+++ src/ExprTree.h
@@ -5,21 +5,77 @@
 #include <concepts>
 #include <optional>
 #include <type_traits>
+#include <tuple>
 
 namespace Utility {
+namespace {template<
+  typename V,
+  typename R,
+  typename... ARGS,
+  typename = std::enable_if_t<std::is_invocable_r_v<R, V, ARGS...>>>
+R evaluate(V v, ARGS... args) {
+  return v(args...);
+}
 
 template<
-  typename OP,
-  typename LEAF,
+  typename V,
   typename R,
-  typename = std::enable_if_t<std::is_invocable_r_v<R, OP, LEAF, LEAF>>,
+  typename... ARGS,
+  typename = std::enable_if_t<!std::is_invocable_r_v<R, V, ARGS...>>,
+  int = 0>
+R evaluate(V v, ARGS... args) {
+  return v;
+}
+}
+
+template<typename T, typename R, typename... ARGS>
+concept NullaryOrPrimitive =
+  std::is_fundamental_v<T> ||
+  requires(T t, ARGS... args) {
+  { t(args...) } -> std::same_as<R>;
+  };
+
+template<typename T, typename ARG, typename... ARGS>
+concept BinOP =
+  requires(T t,
+           std::optional<ARG> arg1,
+           std::optional<ARG> arg2,
+           ARGS... args) {
+  { t(arg1, arg2, args...) } -> std::same_as<ARG>;
+};
+
+template<
+  typename R,
+  typename LEAF,
+  typename OP,
   typename... ARGS>
+requires BinOP<OP, R, ARGS...> &&
+         NullaryOrPrimitive<LEAF, R, ARGS...>
+
 class BinExprTree {
 public:
   enum NodeType {
     OPERATOR,
     OPERAND
   };
+
+  using T_OP__ = std::unique_ptr<OP>;
+  using T_CHILD__ = std::unique_ptr<BinExprTree>;
+
+  BinExprTree() = default;
+  BinExprTree(NodeType type, LEAF v):
+    type_{type}, op_{nullptr},
+    v_{v}, lhs_{nullptr}, rhs_{nullptr} {}
+  BinExprTree(NodeType type, std::unique_ptr<OP> op):
+    type_{type}, op_{std::move(op)},
+    v_{}, lhs_{nullptr}, rhs_{nullptr} {}
+
+  void setL(std::unique_ptr<BinExprTree> t) {
+    lhs_ = std::move(t);
+  }
+  void setR(std::unique_ptr<BinExprTree> t) {
+    rhs_ = std::move(t);
+  }
 
   // nullopt indicate that evaluate is failed.
   std::optional<R> operator()(ARGS... args) {
@@ -28,34 +84,36 @@ public:
       std::optional<R> loperand = std::nullopt;
       std::optional<R> roperand = std::nullopt;
 
-      for (auto& [e, o]: {{lhs_, loperand}, {rhs_, roperand}}) {
-        if (!e == nullptr) {
-          o = (*e)(args...);
-          if (!o.has_value()) {
+      auto t1 = std::tie(lhs_, loperand);
+      auto t2 = std::tie(rhs_, roperand);
+      for (auto& [e, o]: {t1, t2}) {
+        if (!(e == nullptr)) {
+          std::optional<R> tmp = (*e)(args...);
+          if (!tmp.has_value()) {
             return std::nullopt;
+          } else {
+            o = tmp;
           }
         }
       }
 
-      return op_(loperand.value(), roperand.value());
-    } if (type_ == OPERAND) {
-      if (std::is_invocable_r_v<R, LEAF, ARGS...>) {
-        return v_(args...);
-      } else {
-        /* The leaf is an first-order term, can only be
-         * evaluated in another context, just return it. */
-        if (!std::is_convertible_v<LEAF, R>) {
-          return std::nullopt;
-        } else {
-          return v_;
+      if (!loperand.has_value() && !roperand.has_value()) {
+        return std::nullopt;
       }
+
+      return (*op_)(loperand, roperand, args...);
+    } else {
+      if (!std::is_convertible_v<LEAF, R>) {
+        return std::nullopt;
+      } else {
+        return evaluate<LEAF,R>(v_, args...);
       }
     }
-  };
+  }
 
 private:
   NodeType type_;
-  OP op_;
+  std::unique_ptr<OP> op_;
   LEAF v_;
   std::unique_ptr<BinExprTree> lhs_;
   std::unique_ptr<BinExprTree> rhs_;
---
+++ src/ExprTree_unittests.cc
@@ -0,0 +1,109 @@
+#include <gtest/gtest.h>
+#include <rapidcheck/gtest.h>
+
+#include "ExprTree.h"
+
+namespace Utility {
+
+struct IntegerOP {
+  virtual ~IntegerOP() {};
+  virtual int operator()(std::optional<int> lhs,
+                         std::optional<int> rhs,
+                         int* count)
+    { return lhs.value(); };
+};
+
+struct IntegerPlus: public IntegerOP {
+  ~IntegerPlus() {};
+  int operator()(std::optional<int> lhs,
+                 std::optional<int> rhs,
+                 int* count) override {
+    assert(lhs.has_value() && rhs.has_value());
+    ++(*count);
+    return lhs.value() + rhs.value();
+  }
+};
+
+struct IntegerMulti: public IntegerOP {
+  ~IntegerMulti() {};
+  int operator()(std::optional<int> lhs,
+                 std::optional<int> rhs,
+                 int* count) override {
+    assert(lhs.has_value() && rhs.has_value());
+    ++(*count);
+    return lhs.value() * rhs.value();
+  }
+};
+
+struct PlusOne: public IntegerOP {
+  ~PlusOne() {};
+  int operator()(std::optional<int> lhs,
+                 std::optional<int> rhs,
+                 int* count) override {
+    assert(lhs.has_value());
+    ++(*count);
+    return lhs.value() + 1;
+  }
+};
+
+struct ExprTreeTests: public ::testing::Test {
+  void SetUp() override {
+
+  }
+};
+
+using IntExprTree = BinExprTree<int,int,IntegerOP, int*>;
+TEST(ExprTreeTests, Basics) {
+  // +(1,2)
+
+  // Spawn root
+  IntExprTree tree{
+    IntExprTree::OPERATOR,
+    std::make_unique<IntegerPlus>()};
+  // Then give two operands to this oeprator.
+  tree.setL(std::make_unique<IntExprTree>(IntExprTree::OPERAND, 1));
+  tree.setR(std::make_unique<IntExprTree>(IntExprTree::OPERAND, 2));
+
+  int count = 0;
+
+  // Evaluate the expression
+  ASSERT_EQ(3, tree(&count));
+  ASSERT_EQ(1, count);
+}
+
+TEST(ExprTreeTests, WithUnary) {
+  // +(+(1,2),++1)
+
+  // Sawn root
+  IntExprTree tree{
+    IntExprTree::OPERATOR,
+    std::make_unique<IntegerPlus>()};
+
+  // Left child
+  std::unique_ptr<IntExprTree> subPlus =
+    std::make_unique<IntExprTree>(
+      IntExprTree::OPERATOR,
+      std::make_unique<IntegerPlus>());
+  subPlus->setL(std::make_unique<IntExprTree>(IntExprTree::OPERAND, 1));
+  subPlus->setR(std::make_unique<IntExprTree>(IntExprTree::OPERAND, 2));
+
+  // Right child
+  std::unique_ptr<IntExprTree> subPlusOne =
+    std::make_unique<IntExprTree>(
+      IntExprTree::OPERATOR,
+      std::make_unique<PlusOne>());
+  subPlusOne->setL(std::make_unique<IntExprTree>(IntExprTree::OPERAND, 1));
+
+  tree.setL(std::move(subPlus));
+  tree.setR(std::move(subPlusOne));
+
+  int count = 0;
+  ASSERT_EQ(5, tree(&count));
+  ASSERT_EQ(3, count);
+}
+
+TEST(ExprTreeTests, SideEffectLeaf) {
+
+}
+
+} // Utility
