145bd45447c914319551ad89ceca7807a2577e06
NzSN <nzsn0101@gmail.com>
2024-02-25 10:28:21 +0800

    update gc unittests.

--- src/Base/gc_unittests.cc
+++ src/Base/gc_unittests.cc
@@ -1,13 +1,19 @@
 #include <gtest/gtest.h>
+#include <sstream>
 #include "gc_defines.h"
+
+#include "langs.h"
+#include "Parser/Parser-inl.h"
+#include "TransEngine/SigmaTerm.h"
 
 #if ENABLE_GC
 
 namespace Chameleon::Base::GC {
 
 template<typename T>
-struct NumSeq : public GCObject {
-  NumSeq(T a): v{a} {}
+struct NumSeq : public GCObject,
+                public ContextInfo {
+  NumSeq(T a): v{a}, ContextInfo(typeid(NumSeq<T>).name()) {}
   T v;
 
   operator T() const {
@@ -29,7 +35,16 @@ struct NumSeq : public GCObject {
 };
 
 struct Storage {
-  std::vector<NumSeq<int>*> nums;
+  NumSeq<int>* nums() { return nums_; }
+  void setNums(NumSeq<int>* nums) { nums_ = nums; }
+
+  Storage* another() { return another_.get(); }
+  void setAnother(std::unique_ptr<Storage>&& another) {
+    another_ = std::move(another);
+  }
+private:
+  NumSeq<int>* nums_;
+  std::unique_ptr<Storage> another_;
 };
 
 TEST(GCObject, Basic) {
@@ -43,21 +58,71 @@ TEST(GCObject, Basic) {
     NumSeq<int>* seq_2 = cppgc::MakeGarbageCollected<NumSeq<int>>(
       heap->GetAllocationHandle(), 2);
 
-    std::vector<Storage> storages{};
-    storages.push_back(Storage{});
+    std::unique_ptr<Storage> storage_0 = std::make_unique<Storage>();
+    storage_0->setAnother(std::make_unique<Storage>());
+    storage_0->another()->setAnother(std::make_unique<Storage>());
 
-    storages.front().nums.push_back(seq_1);
+    storage_0->another()->setNums(seq_1);
+    storage_0->another()->another()->setNums(seq_2);
 
     seq_1->reach(seq_2);
     seq_1 = nullptr;
     seq_2 = nullptr;
 
     heap->ForceGarbageCollectionSlow("CppGC example", "Testing");
-    seq_1 = storages.front().nums.front();
+    seq_1 = storage_0->another()->nums();
 
-    EXPECT_TRUE(seq_1 == 3);
+    EXPECT_TRUE(*seq_1 == 3);
   }
+
+  cppgc::ShutdownProcess();
 }
+
+// TEST(ParseTreeWithGC, Basic) {
+//   auto cppgc_platform = std::make_shared<cppgc::DefaultPlatform>();
+//   cppgc::InitializeProcess(cppgc_platform->GetPageAllocator());
+
+//   {
+//     std::unique_ptr<cppgc::Heap> heap = cppgc::Heap::Create(cppgc_platform);
+//     Base::GC::Process::gc_heap = heap.get();
+
+//     std::istringstream s{"a + b"};
+//     auto t = Parser::ParserSelect<GET_LANG_TYPE(TESTLANG)>
+//       ::parser
+//       ::parse<Base::ParseTree<Base::Antlr4Node>>(&s);
+//     auto t_copy = t.getMeta().clone();
+
+//     heap->ForceGarbageCollectionSlow("ParseTreeWithGC.Basic", "Test");
+//     t_copy->getText();
+//   }
+//}
+
+// TEST(ParseTreeWithGC, Basic_1) {
+//   auto cppgc_platform = std::make_shared<cppgc::DefaultPlatform>();
+//   cppgc::InitializeProcess(cppgc_platform->GetPageAllocator());
+
+//   {
+//     std::unique_ptr<cppgc::Heap> heap = cppgc::Heap::Create(cppgc_platform);
+//     Base::GC::Process::gc_heap = heap.get();
+
+//     std::istringstream s{"a+b"};
+//     auto env = Utility::Antlr4Parse<TestLangLexer, TestLangParser>(s);
+//     auto tree = env->parser.prog();
+
+//     Base::Antlr4Node nodes{Base::TESTLANG, tree};
+//     std::unique_ptr<Base::Antlr4Node> copy = nodes.clone();
+
+//     auto gcObj = dynamic_cast<Base::GC::GCObject*>(copy->tree());
+//     EXPECT_TRUE(gcObj->objs().size() == 1);
+//     gcObj = nullptr;
+
+//     heap->ForceGarbageCollectionSlow("ParseTreeWithGC.Basic", "Test");
+//     std::cout << copy->getText() << std::endl;
+//   }
+// }
+
+//   cppgc::ShutdownProcess();
+// }
 
 
 } // Chameleon::Base::GC
